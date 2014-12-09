8b208656be6cdfd50bff10b8df809b0e34dd8404
Trahim4ikVlad <vlad.trahim4ik@mail.ru>
2014-12-09 19:53:28 +0300

    Update - 1

---
+++ SystemSalesStatistics/DataAccessLayer/GenericDataRepository.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class GenericDataRepository<T>:IGenericDataRepository<T> where T: class
+    {
+        public virtual IList<T> GetAll(params Expression<Func<T, object>>[] navigationProperties)
+        {
+            List<T> list = null;
+            
+            using (var context = new EntitiesDB())
+            {
+                IQueryable<T> dbQuery = context.Set<T>();
+
+                foreach (var navigationProperty in navigationProperties)
+                {
+                    dbQuery = dbQuery.Include<T, object>(navigationProperty);
+                    list = dbQuery.AsNoTracking().ToList<T>();
+                }
+            }
+
+            return list;
+        }
+
+        public virtual IList<T> GetList(Func<T, bool> @where, params Expression<Func<T, object>>[] navigationProperties)
+        {
+            List<T> list = null;
+
+            using (var contex = new EntitiesDB())
+            {
+                IQueryable<T> dbQuery = contex.Set<T>();
+
+                foreach (Expression<Func<T, object>> navigationProperty in navigationProperties)
+                    dbQuery = dbQuery.Include<T, object>(navigationProperty);
+
+                list = dbQuery
+                    .AsNoTracking()
+                    .Where(where)
+                    .ToList<T>();
+            }
+
+            return list;
+        }
+
+        public virtual T GetSingle(Func<T, bool> where, params Expression<Func<T, object>>[] navigationProperties)
+        {
+            T item = null;
+
+            using (var context = new EntitiesDB())
+            {
+                IQueryable<T> dbQuery = context.Set<T>();
+
+                foreach (Expression<Func<T, object>> navigationProperty in navigationProperties)
+                    dbQuery = dbQuery.Include<T, object>(navigationProperty);
+
+                item = dbQuery
+                    .AsNoTracking() //Не отслеживать любые изменения для выбранного элемента
+                    .FirstOrDefault(where); 
+            }
+            return item;
+        }
+
+        public void Add(params T[] items)
+        {
+            using (var context = new EntitiesDB())
+            {
+                foreach (T item in items)
+                {
+                    context.Entry(item).State = System.Data.EntityState.Added;
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(params T[] items)
+        {
+            using (var context = new EntitiesDB())
+            {
+                foreach (T item in items)
+                {
+                    context.Entry(item).State = System.Data.EntityState.Modified;
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public void Remove(params T[] items)
+        {
+            using (var context = new EntitiesDB())
+            {
+                foreach (T item in items)
+                {
+                    context.Entry(item).State = System.Data.EntityState.Deleted;
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
